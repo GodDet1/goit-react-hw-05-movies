4ac3be75d9be06e081b3544f18d2d29ced6ddee2
myusername <tofeee63@gmail.com>
2022-07-24 16:00:56 +0300

    End

--- src/components/FilmsPage/FilmsPage.jsx
+++ src/components/FilmsPage/FilmsPage.jsx
@@ -1,47 +1,38 @@
 import { fetchfilmById } from 'API/API';
 import SelectedFilm from 'components/SelectedFilm/SelectedFilm';
 import { Suspense, useEffect, useState } from 'react';
-import {
-  Link,
-  Outlet,
-  useLocation,
-  useNavigate,
-  useParams,
-} from 'react-router-dom';
+import { Link, Outlet, useLocation, useParams } from 'react-router-dom';
 import styled from './styled.module.scss';
 
 function FilmsPage() {
   const { postId } = useParams();
   const [film, setFilm] = useState({});
   const { state } = useLocation();
-  const navigate = useNavigate();
 
-  console.log(state);
+  const path = state?.from ?? '/';
 
   useEffect(() => {
     fetchfilmById(postId).then(({ data }) => setFilm(data));
   }, [postId]);
 
-  const handleClick = () =>
-    state === null ? navigate('/') : navigate(state.from);
-
   return (
     <>
       <section className={styled.container + ' container'}>
-        <button type="button" onClick={handleClick} className={styled.btn}>
-          Go back
-        </button>
+        <Link to={path}>
+          <button type="button">Go back</button>
+        </Link>
+
         <SelectedFilm data={film} />
         <hr />
         <h3 className={styled.h3}>Additional info</h3>
         <ul className={styled.link_list}>
           <li>
-            <Link to="cast" state={{ from: state?.from }}>
+            <Link to="cast" state={{ from: path }}>
               Cast
             </Link>
           </li>
           <li>
-            <Link to="rewiews" state={{ from: state?.from }}>
+            <Link to="rewiews" state={{ from: path }}>
               Rewiews
             </Link>
           </li>
